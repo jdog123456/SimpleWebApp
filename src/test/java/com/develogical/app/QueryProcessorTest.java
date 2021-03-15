2777caaa4b566d357c88f64339c6c42654fdd6dd
James Sarkar <j.sarkar@sap.com>
2021-03-15 10:37:41 -0400

    break a test

--- src/test/java/com/develogical/app/QueryProcessorTest.java
+++ src/test/java/com/develogical/app/QueryProcessorTest.java
@@ -11,7 +11,7 @@ public class QueryProcessorTest {
 
     @Test
     public void returnsEmptyStringIfCannotProcessQuery() throws Exception {
-        assertThat(queryProcessor.process("test"), is(""));
+        assertThat(queryProcessor.process("test"), is("not null"));
     }
 
     @Test
